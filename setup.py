3afd65533add19b0dfd27cbeab19f1e6837d3e3a
pconesa <p.conesa.mingo@gmail.com>
2019-03-27 16:56:58 +0100

    version 1.0.3 (with protocols.conf)

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ setup(
     # MANIFEST.in as well.
     # include_package_data=True,
     package_data={  # Optional
-       'powerfit': ['powerfit_logo.png', 'protocols.conf'],
+       'powerfit_scipion': ['powerfit_logo.png', 'protocols.conf'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
