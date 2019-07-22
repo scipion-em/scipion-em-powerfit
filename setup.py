71620729a392cc4c55b089eed0170a0235e6b8f8
marta <mmmtnez@cnb.csic.es>
2019-07-22 16:34:44 +0200

    prepare for pypi

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.5',  # Required
+    version='1.0.6',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -136,7 +136,7 @@ setup(
     #
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
-    # include_package_data=True,
+    include_package_data=True,
     package_data={  # Optional
        'powerfit_scipion': ['powerfit_logo.png', 'protocols.conf'],
     },
