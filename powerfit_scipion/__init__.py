8b60d525cd6bfaec0a7915f5852fe64f87ec9373
Carlos Oscar Sorzano <coss@cnb.csic.es>
2020-05-05 15:35:53 +0200

    Better adapted to Scipion3, but not yet

--- powerfit_scipion/__init__.py
+++ powerfit_scipion/__init__.py
@@ -63,7 +63,6 @@ class Plugin(pwem.Plugin):
                        tar='powerfit.tgz',
                        targets=['powerfit-2.0*'],
                        pythonMod=True,
-                       deps=['numpy', 'scipy', 'fftw3'],
                        default=True)
 
 
